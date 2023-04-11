df5339be991e8edb7dfcc51096d15221283bce04
draker4 <baptiste.perriolat@live.fr>
2023-04-11 12:52:04 +0200

    Message class

--- incl/Message.hpp
+++ incl/Message.hpp
@@ -6,40 +6,53 @@
 /*   By: bperriol <bperriol@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/10 16:21:27 by bperriol          #+#    #+#             */
-/*   Updated: 2023/04/10 19:56:29 by bperriol         ###   ########lyon.fr   */
+/*   Updated: 2023/04/11 12:51:34 by bperriol         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MESSAGE_HPP
 # define MESSAGE_HPP
 
-# include <iostream>
+// # include "ircserv.hpp"
+
+# include <algorithm>
 # include <vector>
+# include <iostream>
 
 class	Message
 {
+	public:
+		typedef	std::vector<std::string>			vecString;
+		typedef	std::vector<std::string>::iterator	itVecString;
+		typedef	std::string::iterator				itString;
+
+		Message(std::string message);
+		~Message(void);
+		
+		Message(const Message &src);
+		
+		Message	&operator=(const Message &rhs);
+
+		vecString	getTags(void) const;
+		std::string	getSource(void) const;
+		std::string	getCommand(void) const;
+		vecString	getParameters(void) const;
+		
+		class ErrorMsgFormat : public std::exception
+		{
+			public:
+				virtual const char	*what() const throw() { return "ERROR: Message not correctly formated!"; }
+		};
+	
 	private:
 		Message(void);
 
-		std::vector<std::string>	_tags;
-		std::string					_source;
-		std::string					_command;
-		std::vector<std::string>	_parameters;
+		vecString	_tags;
+		std::string	_source;
+		std::string	_command;
+		vecString	_parameters;
 		
-		std::vector<std::string>	split(std::string message, std::string character);
-
-	public:
-	Message(std::string message);
-	~Message(void);
-	
-	Message(const Message &src);
-	
-	Message	&operator=(const Message &rhs);
-
-	std::string getTags(void) const;
-	std::string getSource(void) const;
-	std::string getCommand(void) const;
-	std::string getParameters(void) const;
+		vecString	_split(std::string message, std::string character);
 };
 
 #endif
--- srcs/Message.cpp
+++ srcs/Message.cpp
@@ -6,7 +6,7 @@
 /*   By: bperriol <bperriol@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/10 16:22:07 by bperriol          #+#    #+#             */
-/*   Updated: 2023/04/10 20:01:25 by bperriol         ###   ########lyon.fr   */
+/*   Updated: 2023/04/11 12:51:14 by bperriol         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,83 +19,52 @@ Message::Message(void)
 
 Message::Message(std::string message) : _source("")
 {
-	std::vector<std::string>	vec = split(message, " ");
+	if (message.empty() || message[0] == ' ')
+		throw ErrorMsgFormat();
+
+	vecString	vec = _split(message, " ");
 
 	if (vec.empty())
 		throw ErrorMsgFormat();
 	
-	if (vec[0][0] == '@')
-		_tags = split(first, ";")
-	if (first[0] == ':' || (!_tags.empty() && vec[1][0] == ':'))
-		//continue here
-
-	size_t	pos = 0;
-	size_t	prev = 0;
-	
-
-
-	if (message[0] == '@')
-	{
-		pos = message.find_first_of(" ");
-		while (prev < pos)
-		{
-			size_t	tmp = pos;
-			if (pos = message.find_first_of(";", prev) != std::string::npos && pos < tmp)
-			{
-				_tags.push_back(message.substr(prev, pos - 1));
-				prev = pos + 1;
-				if (message[prev] == ' ')
-					throw ErrorMsgFormat();
-			}
-			else
-			{
-				_tags.push_back(message.substr(prev, tmp - 1));
-				prev = tmp;
-			}
-			pos = tmp;
-		}
+	// parse tags
+	if (vec[0][0] == '@') {
+		_tags = _split(vec[0], ";");
+		_tags[0] = _tags[0].substr(1, _tags[0].length());
+		vec.erase(vec.begin());
 	}
-	while (message[pos] == ' ')
-		pos++;
-	if (message[pos] == ':')
-	{
-		prev = pos;
-		pos = message.find_first_of(" ", pos);
-		_source = message.substr(prev + 1, pos - 1);
-		if (_tags.empty() && message[0] != ':')
+	
+	// parse source
+	if (vec[0][0] == ':') {
+		_source = vec[0];
+		_source = _source.substr(1, _source.length());
+		vec.erase(vec.begin());
+	}
+	
+	// parse command
+	_command = vec[0];
+	vec.erase(vec.begin());
+	for (itString it = _command.begin(); it != _command.end(); it++) {
+		if (!isalnum(*it))
 			throw ErrorMsgFormat();
 	}
-	while (message[pos] == ' ')
-		pos++;
-	prev = pos;
-	if ((pos = message.find_first_of(" ")) != std::string::npos)
-	{
-		_command = message.substr(prev, pos - 1);
-		while (message[pos] == ' ')
-			pos++;
-		prev = pos;
-		pos = message.find_first_of("\r\n");
-		while (prev < pos)
-		{
-			size_t	tmp = pos;
-			if (pos = message.find_first_of(" ", prev) != std::string::npos && pos < tmp)
-			{
-				_tags.push_back(message.substr(prev, pos - 1));
-				prev = pos + 1;
-				if (message[prev] == ' ')
-					throw ErrorMsgFormat();
-			}
-				else
-				{
-					_tags.push_back(message.substr(prev, tmp - 1));
-					prev = tmp;
-				}
-				pos = tmp;
-			}
-		}
+	
+	// parse parameters
+	for (itVecString it = vec.begin(); it != vec.end(); it++) {
+		if ((*it)[0] == ':')
+			(*it) = (*it).substr(1, (*it).length());
+		_parameters.push_back(*it);
 	}
-	else
-		_command = message.substr(prev, message.find_first_of("\r\n") - 1);
+
+	// print
+	// for (std::vector<std::string>::iterator it = _tags.begin(); it != _tags.end(); it++) {
+	// 	std::cout << "tags = " << *it << std::endl;
+	// }
+	// std::cout << "source = " << _source << std::endl;
+	// std::cout << "command = " << _command << std::endl;
+	// for (std::vector<std::string>::iterator it = _parameters.begin(); it != _parameters.end(); it++) {
+	// 	std::cout << "parameters = " << *it << std::endl;
+	// }
 }
 
 Message::Message(const Message &src)
@@ -122,7 +91,7 @@ Message	&Message::operator=(const Message &rhs)
 
 /* --------------------------------  Getter  -------------------------------- */
 
-std::string	Message::getTags(void) const
+std::vector<std::string>	Message::getTags(void) const
 {
 	return _tags;
 }
@@ -132,12 +101,12 @@ std::string	Message::getSource(void) const
 	return _source;
 }
 
-std::string	Message::getComman(void) const
+std::string	Message::getCommand(void) const
 {
 	return _command;
 }
 
-std::string	Message::getParameters(void) const
+std::vector<std::string>	Message::getParameters(void) const
 {
 	return _parameters;
 }
@@ -146,20 +115,37 @@ std::string	Message::getParameters(void) const
 
 /* ----------------------  Private member functions  ------------------------ */
 
-std::vector<std::string>	Message::split(std::string message, std::string character)
+std::vector<std::string>	Message::_split(std::string message, std::string character)
 {
 	std::vector<std::string>	result;
-	size_t						pos = 0;
-	size_t						prev = pos;
+	bool						keep_spaces = false;
+	size_t						pos = -1;
+	size_t						prev = 0;
 
 	while ((pos = message.find_first_of(character, pos + 1)) != std::string::npos)
 	{
-		result.push_back(message.substr(prev, pos -1));
+		if (keep_spaces && message.substr(prev, pos - prev)[0] == ':')
+			break ;
+		result.push_back(message.substr(prev, pos - prev));
+		if (result.back()[0] != '@' && result.back()[0] != ':')
+			keep_spaces = true;
 		prev = pos + 1;
 	}
 	pos = message.find_first_of("\r\n");
-	result.push_back(message.substr(prev, pos - 1));
+	result.push_back(message.substr(prev, pos - prev));
 	return result;
 }
 
 /* -----------------------  Public member functions  ------------------------ */
+
+// int main(void)
+// {
+// 	std::string	str = "@ceci;est;un;tag :hahaouiiii allo coucou toi :ouahh ca     marche\r\n";
+// 	// std::string	str = "PASS\r\n";
+// 	try {
+// 		Message	message(str);
+// 	}
+// 	catch(const std::exception &e) {
+// 		std::cout << e.what() << std::endl;
+// 	}
+// }
