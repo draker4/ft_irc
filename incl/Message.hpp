6c6e2f723b1842a5a75cea82bc33038c5da799c3
draker4 <baptiste.perriolat@live.fr>
2023-04-11 10:15:52 +0200

    test

--- incl/Message.hpp
+++ incl/Message.hpp
@@ -6,7 +6,7 @@
 /*   By: bperriol <bperriol@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/10 16:21:27 by bperriol          #+#    #+#             */
-/*   Updated: 2023/04/10 16:40:54 by bperriol         ###   ########lyon.fr   */
+/*   Updated: 2023/04/10 19:56:29 by bperriol         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,19 @@
 # define MESSAGE_HPP
 
 # include <iostream>
+# include <vector>
 
 class	Message
 {
 	private:
 		Message(void);
 
-		std::string	_tags;
-		std::string	_source;
-		std::string	_command;
-		std::string	_parameters;
+		std::vector<std::string>	_tags;
+		std::string					_source;
+		std::string					_command;
+		std::vector<std::string>	_parameters;
+		
+		std::vector<std::string>	split(std::string message, std::string character);
 
 	public:
 	Message(std::string message);
--- srcs/Message.cpp
+++ srcs/Message.cpp
@@ -6,7 +6,7 @@
 /*   By: bperriol <bperriol@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/10 16:22:07 by bperriol          #+#    #+#             */
-/*   Updated: 2023/04/10 17:07:46 by bperriol         ###   ########lyon.fr   */
+/*   Updated: 2023/04/10 20:01:25 by bperriol         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,33 +17,82 @@
 Message::Message(void)
 {}
 
-Message::Message(std::string message) : _tags(""), _source(""), _command(""), _parameters("")
+Message::Message(std::string message) : _source("")
 {
+	std::vector<std::string>	vec = split(message, " ");
+
+	if (vec.empty())
+		throw ErrorMsgFormat();
+	
+	if (vec[0][0] == '@')
+		_tags = split(first, ";")
+	if (first[0] == ':' || (!_tags.empty() && vec[1][0] == ':'))
+		//continue here
+
 	size_t	pos = 0;
 	size_t	prev = 0;
+	
+
 
 	if (message[0] == '@')
 	{
 		pos = message.find_first_of(" ");
-		_tags = message.substr(0, pos - 1);
+		while (prev < pos)
+		{
+			size_t	tmp = pos;
+			if (pos = message.find_first_of(";", prev) != std::string::npos && pos < tmp)
+			{
+				_tags.push_back(message.substr(prev, pos - 1));
+				prev = pos + 1;
+				if (message[prev] == ' ')
+					throw ErrorMsgFormat();
+			}
+			else
+			{
+				_tags.push_back(message.substr(prev, tmp - 1));
+				prev = tmp;
+			}
+			pos = tmp;
+		}
 	}
-	while (message[pos] == " ")
+	while (message[pos] == ' ')
 		pos++;
 	if (message[pos] == ':')
 	{
 		prev = pos;
 		pos = message.find_first_of(" ", pos);
-		_source = message.substr(prev, pos - 1);
+		_source = message.substr(prev + 1, pos - 1);
+		if (_tags.empty() && message[0] != ':')
+			throw ErrorMsgFormat();
 	}
-	while (message[pos] == " ")
+	while (message[pos] == ' ')
 		pos++;
 	prev = pos;
 	if ((pos = message.find_first_of(" ")) != std::string::npos)
 	{
 		_command = message.substr(prev, pos - 1);
-		while (message[pos] == " ")
+		while (message[pos] == ' ')
 			pos++;
-		_parameters = message.substr(pos, message.find_first_of("\r\n") - 1);
+		prev = pos;
+		pos = message.find_first_of("\r\n");
+		while (prev < pos)
+		{
+			size_t	tmp = pos;
+			if (pos = message.find_first_of(" ", prev) != std::string::npos && pos < tmp)
+			{
+				_tags.push_back(message.substr(prev, pos - 1));
+				prev = pos + 1;
+				if (message[prev] == ' ')
+					throw ErrorMsgFormat();
+			}
+				else
+				{
+					_tags.push_back(message.substr(prev, tmp - 1));
+					prev = tmp;
+				}
+				pos = tmp;
+			}
+		}
 	}
 	else
 		_command = message.substr(prev, message.find_first_of("\r\n") - 1);
@@ -96,5 +145,21 @@ std::string	Message::getParameters(void) const
 /* --------------------------------  Setter  -------------------------------- */
 
 /* ----------------------  Private member functions  ------------------------ */
+
+std::vector<std::string>	Message::split(std::string message, std::string character)
+{
+	std::vector<std::string>	result;
+	size_t						pos = 0;
+	size_t						prev = pos;
+
+	while ((pos = message.find_first_of(character, pos + 1)) != std::string::npos)
+	{
+		result.push_back(message.substr(prev, pos -1));
+		prev = pos + 1;
+	}
+	pos = message.find_first_of("\r\n");
+	result.push_back(message.substr(prev, pos - 1));
+	return result;
+}
 
 /* -----------------------  Public member functions  ------------------------ */
