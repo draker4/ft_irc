13f3cb3c852bbad63ab2f174203541dfffe9f2e2
draker4 <baptiste.perriolat@live.fr>
2023-04-27 15:36:23 +0200

    notice ok

--- srcs/command/notice.cpp
+++ srcs/command/notice.cpp
@@ -6,7 +6,7 @@
 /*   By: bperriol <bperriol@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/07 15:13:13 by baptiste          #+#    #+#             */
-/*   Updated: 2023/04/27 15:16:57 by bperriol         ###   ########lyon.fr   */
+/*   Updated: 2023/04/27 15:36:02 by bperriol         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,109 +26,206 @@
  * 
  */
 
-// static bool	is_target_channel(char c, std::string str)
-// {
-// 	size_t	pos = str.find_first_of(c);
-// 	if (pos != std::string::npos)
-// 		return true;
-// 	return false;
-// }
 
-// static Server::vecClient	clients_from_channel(Client *client, Server *server, std::string target, std::string message)
-// {
-// 	int	i = 0;
-// 	int	order = 0;
-// 	std::string	check_double = "";
-// 	Server::vecClient	clients_to_add;
+static std::string	is_target_host(Server *server, std::string str)
+{
+	size_t	pos = str.find_first_of('#');
+	size_t	pos_point = str.find_last_of('.');
+	if (pos == std::string::npos || pos_point == std::string::npos
+		|| pos_point < pos)
+		return "";
+	
+	std::string	host = str.substr(pos + 1, host.length() - pos - 1);
+	Server::mapClient	clients = server->getClients();
+	for (Server::itMapClient it = clients.begin(); it != clients.end(); it++) {
+		if (it->second->getInet() == host)
+			return host;
+	}
+	return "";
+}
 
-// 	// check prefixes
-// 	while (target[i] != '#' && target[i] != '&' && is_target_channel(target[i], "@%+")) {
+static bool	is_target_channel(char c, std::string str)
+{
+	size_t	pos = str.find_first_of(c);
+	if (pos != std::string::npos)
+		return true;
+	return false;
+}
+
+static void	clients_from_host(Client *client, Server *server, std::string target, std::string host, std::string message)
+{
+	Server::vecClient	to_add;
+
+	// need to be operator to use the host mask
+	if (!client->getModeStatus('o'))
+		return ;
+	
+	// if nick#host format
+	if (target[0] != '#') {
+		std::string	nickname = target.substr(0, target.find_first_of('#'));
 		
-// 		// check for double characters
-// 		if (check_double.find_first_of(target[i]) != std::string::npos)
-// 			return clients_to_add;
-// 		else
-// 			check_double.push_back(target[i]);
+		Client	*to_send = server->getClient(nickname);
+		if (!to_send)
+			return ;
 		
-// 		// find operator order
-// 		if (target[i] == '@' && order == 0)
-// 			order = 1;
-// 		else if (target[i] == '%' && order < 2)
-// 			order = 2;
-// 		else if (target[i] == '+' && order < 3)
-// 			order = 3;
-// 		i++;
-// 	}
-	
-// 	// find name of the channel
-// 	Channel	*channel = server->getChannel(target.substr(i, target.length() - i));
-// 	if (!channel)
-// 		return clients_to_add;
-	
-// 	// no external message mode
-// 	if (channel->getModeStatus('n') && !channel->isClientInChannel(client->getNickName()))
-// 		return clients_to_add;
-
-// 	// moderated channel mode
-// 	if (channel->getModeStatus('m')
-// 		&& channel->getOperGrade(client->getNickName()) == 0)
-// 		return clients_to_add;
-	
-// 	// find clients to add
-// 	Channel::mapClients	channel_clients = channel->getClients();
-// 	for (Channel::itMapClients it = channel_clients.begin(); it != channel_clients.end(); it++) {
-// 		if (it->second.client == client)
-// 			continue;
-// 		else if (it->second.prefix.empty() && order < 1)
-// 			clients_to_add.push_back(it->second.client);
-// 		else if (it->second.prefix == "+" && order < 2)
-// 			clients_to_add.push_back(it->second.client);
-// 		else if (it->second.prefix == "%" && order < 3)
-// 			clients_to_add.push_back(it->second.client);
-// 		else if (it->second.prefix == "@" && order <= 3)
-// 			clients_to_add.push_back(it->second.client);
-// 	}
-	
-// 	return clients_to_add;
-// }
-
-// static Client	*client_to_add(Client *client, Server *server, std::string target)
-// {
-// 	// if target has user@host format
-// 	if (target.find_first_of("@") != std::string::npos) {
-
-// 		if (!client->getModeStatus('o'))
-// 			return NULL;
+		if (to_send->getInet() != host)
+			return ;
 		
-// 		size_t	pos = target.find_last_of(".");
-// 		if (pos == std::string::npos
-// 			|| (target[pos + 1] && (target[pos + 1] == '*'
-// 			|| target[pos + 1] == '?')))
-// 			return NULL;
+		server->sendClient(RPL_CMD(client->getNickName(), client->getUserName(),
+		client->getInet(), std::string("PRIVMSG"), to_send->getNickName() + " :" + message), 
+			to_send->getClientSocket());
+		return ;
+	}
 
-// 		Server::vecClient	target_hosts = server->getClientsHost(target);
+	// if #host fromat
+	Server::mapClient	clients = server->getClients();
+
+	for (Server::itMapClient it = clients.begin(); it != clients.end(); it++) {
+		if (it->second->getInet() == host) {
+			server->sendClient(RPL_CMD(client->getNickName(), client->getUserName(),
+			client->getInet(), std::string("PRIVMSG"),
+			it->second->getNickName() + " :" + message), 
+			it->second->getClientSocket());
+		}
+	}
+}
+
+static void	clients_from_channel(Client *client, Server *server, std::string target, std::string message)
+{
+	int	i = 0;
+	int	order = 0;
+	std::string	check_double = "";
+	Server::vecClient	to_send;
+
+	// check prefixes
+	while (target[i] != '#' && target[i] != '&' && is_target_channel(target[i], "@%+")) {
 		
-// 		if (target_hosts.empty())
-// 			return NULL;
-// 		else if (target_hosts.size() > 1)
-// 			return NULL;
-// 		else
-// 			return target_hosts[0];
-// 	}
+		// check for double characters
+		if (check_double.find_first_of(target[i]) != std::string::npos)
+			return ;
+		else
+			check_double.push_back(target[i]);
+		
+		// find operator order
+		if (target[i] == '@' && order == 0)
+			order = 1;
+		else if (target[i] == '%' && order < 2)
+			order = 2;
+		else if (target[i] == '+' && order < 3)
+			order = 3;
+		i++;
+	}
 	
-// 	// if target is a nickname
-// 	Client	*client_to_add = server->getClient(target);
+	// find name of the channel
+	Channel	*channel = server->getChannel(target.substr(i, target.length() - i));
+	if (!channel)
+		return ;
 	
-// 	if (!client_to_add)
-// 			return NULL;
-	
-// 	return client_to_add;
-// }
+	// no external message mode
+	if (channel->getModeStatus('n') && !channel->isClientInChannel(client->getNickName()))
+		return ;
 
-void notice(Client *client, const Message &message, Server *server)
+	// moderated channel mode
+	if (channel->getModeStatus('m')
+		&& channel->getOperGrade(client->getNickName()) == 0)
+		return ;
+
+	// check if voiced or banned
+	
+	// find clients to add
+	Channel::mapClients	channel_clients = channel->getClients();
+	for (Channel::itMapClients it = channel_clients.begin(); it != channel_clients.end(); it++) {
+		if (it->second.client == client)
+			continue;
+		else if (it->second.prefix.empty() && order < 1)
+			to_send.push_back(it->second.client);
+		else if (it->second.prefix == "+" && order < 2)
+			to_send.push_back(it->second.client);
+		else if (it->second.prefix == "%" && order < 3)
+			to_send.push_back(it->second.client);
+		else if (it->second.prefix == "@" && order <= 3)
+			to_send.push_back(it->second.client);
+	}
+	
+	for (Server::itVecClient it_client = to_send.begin(); it_client != to_send.end(); it_client++) {
+		server->sendClient(RPL_CMD(client->getNickName(), client->getUserName(),
+			client->getInet(), std::string("PRIVMSG"),
+			channel->getName() + " :" + message), 
+			(*it_client)->getClientSocket());
+	}
+}
+
+static void	client_from_nick(Client *client, Server *server, std::string target, std::string message)
+{
+	// if target has nickname!*@* format
+	if (target.find_first_of("!") != std::string::npos) {
+		size_t	first_pos = target.find_first_of("!");
+		size_t	second_pos = target.find_first_of("@");
+		
+		if (first_pos == std::string::npos || second_pos == std::string::npos)
+			return ;
+
+		Client	*destination = server->getClient(target.substr(0, first_pos));
+		
+		if (!destination)
+			return ;
+		
+		std::string	username = target.substr(first_pos + 1, second_pos - first_pos - 1);
+		
+		if (destination->getUserName() != username)
+			return ;
+				
+		std::string	host = target.substr(second_pos + 1, target.length() - second_pos);
+		
+		if (destination->getInet() != host)
+			return ;
+		
+		server->sendClient(RPL_CMD(client->getNickName(), client->getUserName(),
+				client->getInet(), std::string("PRIVMSG"),
+				destination->getNickName() + " :" + message), 
+				destination->getClientSocket());
+		return ;
+	}
+	
+	// if target has user@host format
+	if (target.find_first_of("@") != std::string::npos) {
+		
+		size_t	pos = target.find_last_of(".");
+		if (pos == std::string::npos
+			|| (target[pos + 1] && (target[pos + 1] == '*' || target[pos + 1] == '?')))
+			return ;
+
+		Server::vecClient	clients = server->getClientsHost(target);
+		
+		if (clients.empty())
+			return ;
+		
+		if (clients.size() > 1)
+			return ;
+		
+		server->sendClient(RPL_CMD(client->getNickName(), client->getUserName(),
+				client->getInet(), std::string("PRIVMSG"),
+				clients[0]->getNickName() + " :" + message), 
+				clients[0]->getClientSocket());
+		return ;
+	}
+	
+	// if target is a nickname
+	Client	*to_send = server->getClient(target);
+	
+	if (!to_send)
+		return ;
+	
+	server->sendClient(RPL_CMD(client->getNickName(), client->getUserName(),
+				client->getInet(), std::string("PRIVMSG"),
+				to_send->getNickName() + " :" + message), 
+				to_send->getClientSocket());
+}
+
+void privmsg(Client *client, const Message &message, Server *server)
 {
 	if (DEBUG_COMMAND)
-		std::cout << BLUE << "NOTICE command called" << RESET << std::endl;
+		std::cout << BLUE << "PRIVMSG command called" << RESET << std::endl;
 
 	// if no target
 	if (message.getParameters().empty())
@@ -137,34 +234,29 @@ void notice(Client *client, const Message &message, Server *server)
 	// if no text to send
 	if (message.getParameters().size() == 1)
 		return ;
-	(void)client;
-	(void)server;
-// 	// get all targets from recipient in one vector
-// 	Message::vecString	targets = split(message.getParameters()[0], ",");
 	
-// 	// send message to all recipients
-// 	for (Message::itVecString it = targets.begin(); it != targets.end(); it++) {
+	// get all targets from recipient in one vector
+	Message::vecString	targets = split(message.getParameters()[0], ",");
+	
+	// send message to all recipients
+	for (Message::itVecString it = targets.begin(); it != targets.end(); it++) {
 
-// 		// to check if <target> is the name of a client or the name of a channel
-// 		if (is_target_channel(message.getParameters()[0][0], "#&@%+")) {
-			
-// 			// list of users to send the text
-// 			Server::vecClient	list_clients = clients_from_channel(client, server, *it);
-			
-// 			//send message to all targets
-// 			for (Server::itVecClient it_client = list_clients.begin(); it_client != list_clients.end(); it_client++) {
-// 				server->sendClient(RPL_CMD(client->getNickName(), client->getUserName(),
-// 					client->getInet(), std::string("NOTICE"), *it + " :" + message.getParameters()[1]), 
-// 					(*it_client)->getClientSocket());
-// 			}
-// 		}
-// 		else {
-// 			Client	*to_add = client_to_add(client, server, *it);
-// 			if (to_add)
-// 				server->sendClient(RPL_CMD(client->getNickName(), client->getUserName(),
-// 						client->getInet(), std::string("NOTICE"),
-// 						to_add->getNickName() + " :" + message.getParameters()[1]), 
-// 						to_add->getClientSocket());
-// 		}
-// 	}
+		std::string	host = is_target_host(server, (*it));
+
+		if (!host.empty()) {
+			if (DEBUG_COMMAND)
+				std::cout << BLUE << "NOTICE sent to host" << RESET << std::endl;
+			clients_from_host(client, server, *it, host, message.getParameters()[1]);
+		}
+		else if (is_target_channel((*it)[0], "#&@%+")) {
+			if (DEBUG_COMMAND)
+				std::cout << BLUE << "NOTICE sent to channel" << RESET << std::endl;
+			clients_from_channel(client, server, *it, message.getParameters()[1]);
+		}
+		else {
+			if (DEBUG_COMMAND)
+				std::cout << BLUE << "NOTICE sent to nickname" << RESET << std::endl;
+			client_from_nick(client, server, *it, message.getParameters()[1]);
+		}
+	}
 }
